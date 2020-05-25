20d09bb30a07cc55ef34c2c9ec0bff4f6d11fc7b
Dmitriy <dmt.alfa@gmail.com>
2020-05-25 20:31:54 +0600

    login.html added

--- project1/application.py
+++ project1/application.py
@@ -24,3 +24,11 @@ db = scoped_session(sessionmaker(bind=engine))
 @app.route("/")
 def index():
     return render_template("index.html")
+
+@app.route("/register")
+def register():
+    return render_template("register.html")
+
+@app.route("/login")
+def login():
+    return render_template("login.html")
