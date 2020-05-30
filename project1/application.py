f9ff0d1940577d7a56e11f89e3fdd1e6444af2a0
Dmitriy <dmt.alfa@gmail.com>
2020-05-30 16:00:08 +0600

    login and logout done

--- project1/application.py
+++ project1/application.py
@@ -20,17 +20,25 @@ Session(app)
 engine = create_engine(os.getenv("DATABASE_URL"))
 db = scoped_session(sessionmaker(bind=engine))
 
+session={}
 
 @app.route("/")
 def index():
-    return render_template("index.html")
+    print("session:", session)
+    if session.get("user_id") is None:
+        return render_template("index.html", log_status=False)
+    return render_template("index.html", log_status=True, user_name=session["user"][1])
 
 @app.route("/register")
 def register():
-    return render_template("register.html")
+    print("session:", session)
+    if session.get("user_id") is None:
+        return render_template("register.html", log_status=False)
+    return render_template("register.html", log_status=True, user_name=session["user"][1])
 
 @app.route("/check_register", methods=["POST"])
 def check_register():
+    print("session:", session)
     user_name = request.form.get("user_name")
     user_pass = request.form.get("user_pass")
     if db.execute("SELECT * FROM accounts WHERE user_name = :user_name", {"user_name": user_name}).rowcount > 0:
@@ -39,15 +47,58 @@ def check_register():
             {"user_name": user_name, "user_pass": user_pass})
     db.commit()
     return render_template("success_register.html")
+    if session.get("user_id") is None:
+        return render_template("success_register.html", log_status=False)
+    return render_template("success_register.html", log_status=True, user_name=session["user"][1])
 
 @app.route("/login")
 def login():
-    return render_template("login.html")
+    print("session:", session)
+    if session.get("user_id") is None:
+        return render_template("login.html", log_status=False)
+    return render_template("login.html", log_status=True, user_name=session["user"][1])
 
-@app.route("/check_login", methods=["POST"])
+
+@app.route("/check_login", methods=["GET", "POST"])
 def check_login():
+    print("session:", session)
     user_name = request.form.get("user_name")
     user_pass = request.form.get("user_pass")
-    if db.execute("SELECT * FROM accounts WHERE user_name = :user_name AND user_pass = :user_pass", {"user_name": user_name, "user_pass": user_pass}).rowcount == 0:
+    user = db.execute("SELECT id, user_name FROM accounts WHERE user_name = :user_name AND user_pass = :user_pass"
+        , {"user_name": user_name, "user_pass": user_pass})
+    if user.rowcount == 0:
             return render_template("error.html", message="Invalid user name or password")
-    return render_template("success_login.html")
+    for _ in user:
+        session["user_id"] = _[0]
+        session["user_name"] =  _[1]
+        session["user"] = (_[0], _[1])
+    print("session:", session)
+    if session.get("user_id") is None:
+        return render_template("login.html", log_status=False)
+    return render_template("login.html", log_status=True, user_name=session["user"][1])
+
+
+@app.route("/log-out")
+def log_out():
+    del session["user_id"]
+    del session["user_name"]
+    del session["user"]
+    return render_template("index.html", log_status=False)
+
+def check_authorization():
+    if session.get("user_id") is None:
+        print("check_login=> user_id is None!")
+        return False, "", ""
+    else:
+        print("check_login=> user_id is:", session["user_id"])
+        print("check_login=> user_name is:", session["user_name"])
+        return True, session["user_id"], session["user_name"]
+
+def test_delete():
+    user = db.execute("SELECT id, user_name FROM accounts WHERE user_name = 'dima' AND user_pass = 'dima'")
+    print("rowcount:", user.rowcount)
+    for i in user:
+        print("i[0]=", i[0])
+        print("i[1]=", i[1])
+    #print("session:", session)
+#test_delete()
